20dffbf0ab5bc150cc435ebf59539b78cb5d9463
lijianchang <13155820861@163.com>
2016-08-21 17:57:44 +0800

    commit

---
+++ AndroidStudioProjects/MyPracticeWork/app/src/main/java/ljc/mypracticework/fragment/FlowLayoutFragment.java
@@ -0,0 +1,37 @@
+package ljc.mypracticework.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import ljc.mypracticework.R;
+
+/**
+ * Created by lijianchang on 16/8/21.
+ *
+ */
+public class FlowLayoutFragment extends Fragment {
+
+    public static FlowLayoutFragment newInstance(){
+        return new FlowLayoutFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        return super.onCreateView(inflater, container, savedInstanceState);
+        View view  = inflater.inflate(R.layout.fragment_flow,container,false);
+        ButterKnife.bind(this,view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
