18d9ad071e162e5615278185b9d37ce1b7117df2
lijianchang <13155820861@163.com>
2016-08-23 17:37:07 +0800

    第二个功能的完成

--- AndroidStudioProjects/MyPracticeWork/app/src/main/java/ljc/mypracticework/fragment/FlowLayoutFragment.java
+++ AndroidStudioProjects/MyPracticeWork/app/src/main/java/ljc/mypracticework/fragment/FlowLayoutFragment.java
@@ -1,9 +1,11 @@
 package ljc.mypracticework.fragment;
 
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -37,7 +39,10 @@ public class FlowLayoutFragment extends Fragment {
     FlowLayout mContainer;
     @Bind(R.id.fragment_flow_et)
     EditText mEtAdd;
+    @Bind(R.id.fragment_flow_container1)
+    FlowLayout mContainer1;
     List<String> mList = new ArrayList<>();
+    List<String> mListAdd = new ArrayList<>();
     PopupWindow mPopupWindow;
     public static final String TAG = "FlowLayoutFragment";
     @Nullable
@@ -75,6 +80,28 @@ public class FlowLayoutFragment extends Fragment {
                 return false;
             }
         });
+        initView();
+    }
+
+    private void initView() {
+        mListAdd.add("China");
+        mListAdd.add("USA");
+        mListAdd.add("Jap");
+        mContainer1.removeAllViews();
+        for(int i = 0;i<mListAdd.size();i++){
+            final TextView mTvTags = (TextView)LayoutInflater.from(getActivity()).inflate(R.layout.tagview,mContainer,false);
+            mTvTags.setText(mListAdd.get(i));
+            mContainer1.addView(mTvTags);
+            mTvTags.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(!mList.contains(mTvTags.getText().toString())){
+                        mList.add(mTvTags.getText().toString());
+                    }
+                    showTags();
+                }
+            });
+        }
     }
 
     private void showTags() {
@@ -86,18 +113,18 @@ public class FlowLayoutFragment extends Fragment {
             mTvTags.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if((boolean)mTvTags.getTag()){
-                        /**
-                         * 被激活的edittext的点击事件
-                         */
-                        return;
-                    }else{
+//                    if((boolean)mTvTags.getTag()){
+//                        /**
+//                         * 被激活的edittext的点击事件
+//                         */
+//                        return;
+//                    }else{
                         /**
                          * 未被激活的edittext点击事件
                          */
                         mTvTags.setTag(true);
-                        showPopWindow();
-                    }
+                        showPopWindow(mTvTags);
+//                    }
 
                 }
             });
@@ -105,9 +132,47 @@ public class FlowLayoutFragment extends Fragment {
         mContainer.addView(mEtAdd);
     }
 
-    private void showPopWindow() {
-        if(mPopupWindow != null){
-
+    int mPopupWindowWidth;
+    int mPopupWindowHeight;
+    View contentView;
+    private void showPopWindow(final TextView view) {
+        if(mPopupWindow == null){
+            contentView = View.inflate(getActivity(),R.layout.popwindowlayout,null);
+            mPopupWindow = new PopupWindow(contentView,ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+            mPopupWindow.setBackgroundDrawable(new BitmapDrawable());
+            mPopupWindow.setOutsideTouchable(true);
+            mPopupWindow.setTouchable(true);
+            contentView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+            mPopupWindowWidth = contentView.getMeasuredWidth();
+            mPopupWindowHeight = contentView.getMeasuredHeight();
+//            mPopupWindow.setAnimationStyle();
         }
+        TextView mTvDelete = (TextView)contentView.findViewById(R.id.popwindowlayout_delete);
+        TextView mTvCancel = (TextView)contentView.findViewById(R.id.popwindowlayout_reback);
+        mTvDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mList.remove(view.getText());
+                mPopupWindow.dismiss();
+                showTags();
+            }
+        });
+        mTvCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPopupWindow.dismiss();
+            }
+        });
+        int[] location = new int[2];
+        view.getLocationOnScreen(location);
+        int x = location[0];
+        int y = location[1];
+        mPopupWindow.showAtLocation(view, Gravity.NO_GRAVITY,x+view.getWidth()/2-mPopupWindowWidth/2,y-mPopupWindowHeight-10);
+        mPopupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                view.setTag(false);
+            }
+        });
     }
 }
